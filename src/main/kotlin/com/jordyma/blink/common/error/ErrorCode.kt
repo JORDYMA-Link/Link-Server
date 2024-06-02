ac6ffb4177d9568a935e842fe775fffebd99a179
Minju Kim <mindori1019@gmail.com>
2024-06-02 19:10:34 +0900

    feat/error: 예외 처리를 위한 ErrorCode 추가

--- src/main/kotlin/com/jordyma/blink/common/error/ErrorCode.kt
+++ src/main/kotlin/com/jordyma/blink/common/error/ErrorCode.kt
@@ -7,3 +7,6 @@ data class ErrorCode(
 )
 
 val USER_NOT_FOUND = ErrorCode("M1", "해당 사용자를 찾을 수 없습니다")
+val FOLDER_NOT_FOUND = ErrorCode("M1", "해당 폴더를 찾을 수 없습니다")
+val FEEDS_NOT_FOUND = ErrorCode("M1", "해당 피드를 찾을 수 없습니다")
+val KEYWORDS_NOT_FOUND = ErrorCode("M1", "해당 키워드를 찾을 수 없습니다")
