4373b5188368212c3cee9fac6e57818aea1d4968
Minju Kim <mindori1019@gmail.com>
2024-06-02 19:02:50 +0900

    feat/repository: repository 파일 추가

--- src/main/kotlin/com/jordyma/blink/feed/repository/FeedRepository.kt
+++ src/main/kotlin/com/jordyma/blink/feed/repository/FeedRepository.kt
@@ -3,11 +3,8 @@ package com.jordyma.blink.feed.repository
 import com.jordyma.blink.feed.entity.Feed
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
-import org.springframework.data.repository.query.Param
-import java.time.LocalDate
 
-interface FeedRepository : JpaRepository<Feed, Long> {
+interface FeedRepository : JpaRepository<Feed, Long>, CustomFeedRepository {
 
-    @Query("SELECT f FROM Feed f WHERE DATE(f.createdAt) = :date")
-    fun findByCreatedAt(@Param("date") date: LocalDate): List<Feed>
 }
+
--- src/main/kotlin/com/jordyma/blink/folder/repository/FolderRepository.kt
+++ src/main/kotlin/com/jordyma/blink/folder/repository/FolderRepository.kt
@@ -3,5 +3,5 @@ package com.jordyma.blink.folder.repository
 import com.jordyma.blink.folder.entity.Folder
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface FolderRepository : JpaRepository<Folder, Int> {
+interface FolderRepository : JpaRepository<Folder, Long> {
 }
