e8af637d25eaeabfbcfb02fa44f97790a07aca86
Minju Kim <mindori1019@gmail.com>
2024-06-02 19:02:29 +0900

    feat/service: service 파일 추가

---
+++ src/main/kotlin/com/jordyma/blink/user/service/UserService.kt
@@ -0,0 +1,17 @@
+package com.jordyma.blink.user.service
+
+import com.jordyma.blink.common.error.USER_NOT_FOUND
+import com.jordyma.blink.common.error.exception.BadRequestException
+import com.jordyma.blink.user.entity.User
+import com.jordyma.blink.user.repository.UserRepository
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Service
+
+@Service
+class UserService @Autowired constructor(
+    private val userRepository: UserRepository
+){
+    fun find(userId: Long): User {
+        return userRepository.findById(userId).orElseThrow { BadRequestException(USER_NOT_FOUND) }
+    }
+}
