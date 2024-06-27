87b5a10f4bec7078747703c1d1a84190584b9a23
bjih1999 <bjih1999@naver.com>
2024-06-27 22:53:28 +0900

    feat: swagger 설정 변경

--- src/main/kotlin/com/jordyma/blink/auth/dto/request/KakaoLoginRequestDto.kt
+++ src/main/kotlin/com/jordyma/blink/auth/dto/request/KakaoLoginRequestDto.kt
@@ -2,15 +2,7 @@ package com.jordyma.blink.auth.dto.request
 
 import io.swagger.v3.oas.annotations.media.Schema
 import jakarta.validation.constraints.NotNull
-import lombok.AccessLevel
-import lombok.AllArgsConstructor
-import lombok.Getter
-import lombok.NoArgsConstructor
 
-
-//@Getter
-//@AllArgsConstructor
-//@NoArgsConstructor(access = AccessLevel.PRIVATE)
 data class KakaoLoginRequestDto (
     @NotNull
     @Schema(description = "idToken")
