0ad9a8faf5c0c3a3ce383c9699e90b3be32c7fa2
Minju Kim <mindori1019@gmail.com>
2024-06-28 01:11:35 +0900

    feat/feed: api return ResponseEntity로 감싸게 변경

--- src/main/kotlin/com/jordyma/blink/feed/controller/FeedController.kt
+++ src/main/kotlin/com/jordyma/blink/feed/controller/FeedController.kt
@@ -6,6 +6,7 @@ import com.jordyma.blink.feed.service.FeedService
 import com.jordyma.blink.user.entity.User
 import com.jordyma.blink.user.service.UserService
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
@@ -18,8 +19,12 @@ class FeedController @Autowired constructor(
     private val userService: UserService
 ) {
     @GetMapping("")
-    fun getFeedsByDate(@RequestParam("yearMonth") yearMonth: String, @RequestUserId userId: Long): Map<String, FeedCalendarResponseDto> {
+    fun getFeedsByDate(
+        @RequestParam("yearMonth") yearMonth: String,
+        @RequestUserId userId: Long
+    ): ResponseEntity<Map<String, FeedCalendarResponseDto>> {
         val user: User = userService.find(userId)
-        return feedService.getFeedsByMonth(user = user, yrMonth = yearMonth)
+        val response = feedService.getFeedsByMonth(user = user, yrMonth = yearMonth)
+        return ResponseEntity.ok(response)
     }
 }
