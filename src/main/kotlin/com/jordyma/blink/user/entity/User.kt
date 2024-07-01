7725ec10453dedbe184df9ec20476fbc9c926a25
Minju Kim <mindori1019@gmail.com>
2024-07-01 22:32:51 +0900

    feat/user: of 대신 일반 생성자로 객체 생성할 수 있게 변경 (주 생성자에 필수 데이터 넣음)

--- src/main/kotlin/com/jordyma/blink/user/entity/User.kt
+++ src/main/kotlin/com/jordyma/blink/user/entity/User.kt
@@ -2,41 +2,25 @@ package com.jordyma.blink.user.entity
 
 import com.jordyma.blink.global.entity.BaseTimeEntity
 import jakarta.persistence.*
-import lombok.Getter
-import java.time.LocalDateTime
 
-
-@Getter
 @Entity(name = "Member")
-class User: BaseTimeEntity() {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id")
-    var id: Long? = null
-
+class User(
     @Column(name = "nickname")
-    var nickname: String? = null
+    var nickname: String = "",
 
     @Column(name = "social_type")
     @Enumerated(EnumType.STRING)
-    var socialType: SocialType? = null
+    val socialType: SocialType? = null,
 
     @Column(name = "social_user_id")
-    var socialUserId: String? = null
+    val socialUserId: String? = null,
 
     @Column(name = "role")
     @Enumerated(EnumType.STRING)
     var role: Role? = null
 
-    companion object {
-        fun of(nickname: String, socialType: SocialType, socialUserId: String, role: Role): User {
-            val user = User()
-            user.nickname = nickname
-            user.socialType = socialType
-            user.socialUserId = socialUserId
-            user.role = role
-            return user
-        }
-    }
-
+): BaseTimeEntity()  {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    val id: Long? = null
 }
