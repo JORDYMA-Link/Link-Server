436a558d86a3f32f8b6c455eb097951acc8ff10a
Minju Kim <mindori1019@gmail.com>
2024-07-18 00:31:06 +0900

    feat/feed : 피드 상세 조회를 위해 JPA 쿼리 구현

--- src/main/kotlin/com/jordyma/blink/feed/repository/impl/CustomFeedRepositoryImpl.kt
+++ src/main/kotlin/com/jordyma/blink/feed/repository/impl/CustomFeedRepositoryImpl.kt
@@ -1,14 +1,13 @@
 package com.jordyma.blink.feed.repository.impl
 
+import com.jordyma.blink.feed.vo.FeedDetailVo
 import com.jordyma.blink.feed.entity.QFeed
 import com.jordyma.blink.feed.vo.FeedFolderVo
 import com.jordyma.blink.feed.repository.CustomFeedRepository
 import com.jordyma.blink.folder.entity.QFolder
 import com.querydsl.core.types.Projections
 import com.querydsl.jpa.impl.JPAQueryFactory
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Repository
-import java.time.LocalDate
 import java.time.LocalDateTime
 
 @Repository
@@ -39,5 +38,31 @@ class CustomFeedRepositoryImpl(
                     .and(feed.createdAt.between(startOfMonth, endOfMonth))
             )
             .fetch()
+    }
+
+    override fun findFeedDetail(memberId: Long, feedId: Long): FeedDetailVo? {
+        val qFeed = QFeed.feed
+        val qFolder = QFolder.folder
+
+        return queryFactory
+            .select(
+                Projections.constructor(
+                    FeedDetailVo::class.java,
+                    qFeed.id,
+                    qFeed.thumbnailImage,
+                    qFeed.title,
+                    qFeed.createdAt.`as`("date"),
+                    qFeed.summary,
+                    qFolder.name.`as`("folderName"),
+                    qFeed.memo
+                )
+            )
+            .from(qFeed)
+            .join(qFolder).on(qFeed.folder.id.eq(qFolder.id))
+            .where(
+                qFolder.user.id.eq(memberId),
+                qFeed.id.eq(feedId)
+            )
+            .fetchOne()
     }
 }
