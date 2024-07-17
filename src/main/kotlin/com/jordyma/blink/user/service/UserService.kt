11f3419fed2326d15a05eb0f0dec6b95b2ede4e7
Minju Kim <mindori1019@gmail.com>
2024-07-18 00:38:08 +0900

    feat/feed : 에러 처리 부분 ApplicationException으로 통일

--- src/main/kotlin/com/jordyma/blink/user/service/UserService.kt
+++ src/main/kotlin/com/jordyma/blink/user/service/UserService.kt
@@ -1,9 +1,9 @@
 package com.jordyma.blink.user.service
 
 import com.jordyma.blink.global.error.ID_NOT_FOUND
-import com.jordyma.blink.global.error.USER_NOT_FOUND
-import com.jordyma.blink.global.error.exception.BadRequestException
 import com.jordyma.blink.global.error.exception.IdRequiredException
+import com.jordyma.blink.global.exception.ApplicationException
+import com.jordyma.blink.global.exception.ErrorCode
 import com.jordyma.blink.user.dto.UserInfoDto
 import com.jordyma.blink.user.repository.UserRepository
 import org.springframework.stereotype.Service
@@ -12,10 +12,11 @@ import org.springframework.transaction.annotation.Transactional
 @Service
 class UserService(
     private val userRepository: UserRepository
-){
+) {
     @Transactional(readOnly = true)
     fun find(userId: Long): UserInfoDto {
-        val user = userRepository.findById(userId).orElseThrow { BadRequestException(USER_NOT_FOUND) }
+        val user =
+            userRepository.findById(userId).orElseThrow { ApplicationException(ErrorCode.NOT_FOUND, "사용자를 찾을 수 없습니다") }
         return UserInfoDto(
             id = user.id ?: throw IdRequiredException(ID_NOT_FOUND),
             name = user.nickname
