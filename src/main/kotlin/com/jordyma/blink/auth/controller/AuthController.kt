96cc2a68ba663c04fc0b4ae9e3f5b8b46ce10bdc
bjih1999 <bjih1999@naver.com>
2024-07-30 00:40:58 +0900

    feat: 웹 로그인 리디렉션 추가

--- src/main/kotlin/com/jordyma/blink/auth/controller/AuthController.kt
+++ src/main/kotlin/com/jordyma/blink/auth/controller/AuthController.kt
@@ -1,15 +1,23 @@
 package com.jordyma.blink.auth.controller
-import com.jordyma.blink.auth.service.AuthService
+import com.jordyma.blink.auth.dto.State
 import com.jordyma.blink.auth.dto.request.KakaoLoginRequestDto
 import com.jordyma.blink.auth.dto.response.TokenResponseDto
+import com.jordyma.blink.auth.service.AuthService
 import com.jordyma.blink.global.util.CommonUtil
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.media.Schema
 import io.swagger.v3.oas.annotations.tags.Tag
+import kotlinx.serialization.json.*
 import lombok.RequiredArgsConstructor
+import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.*
+import org.springframework.web.util.UriComponentsBuilder
+import java.net.URI
+import java.util.*
+
 
 @Tag(name = "auth", description = "인증 API")
 @RestController
@@ -41,8 +49,31 @@ class AuthController(
 
     @GetMapping("/kakao-login-web/callback")
     fun kakaoLoginWeb(
-        @RequestParam("code") code: String
-    ): ResponseEntity<TokenResponseDto> {
-        return ResponseEntity.ok(authService.kakaoLoginWeb(code))
+        @RequestParam("code") code: String,
+        @RequestParam("state") state: String,
+    ): ResponseEntity<Void> {
+        val base64Decoder = Base64.getUrlDecoder()
+        val jsonFormat = Json { prettyPrint = true }
+        val jsonString = base64Decoder.decode(state).toString(Charsets.UTF_8)
+        val stateInfo: State = runCatching {
+            jsonFormat.decodeFromString<State>(jsonString)
+        }.getOrElse {
+            State(
+                // TODO 수정
+                webRedirectUrl = "https://blink.jordyma.com",
+            )
+        }
+        val webRedirectUrl = stateInfo.webRedirectUrl
+        val tokenInfo = authService.kakaoLoginWeb(code)
+        val redirectUrl = "${webRedirectUrl}?accessToken=${tokenInfo.accessToken}&refreshToken=${tokenInfo.refreshToken}"
+        val uri = UriComponentsBuilder.newInstance()
+            .host(redirectUrl)
+            .queryParam("accessToken", tokenInfo.accessToken)
+            .queryParam("refreshToken", tokenInfo.refreshToken)
+            .build()
+
+        val headers = HttpHeaders()
+        headers.location = uri.toUri()
+        return ResponseEntity<Void>(headers, HttpStatus.FOUND)
     }
 }
