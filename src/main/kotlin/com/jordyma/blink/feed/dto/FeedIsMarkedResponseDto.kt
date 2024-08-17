357fe7cfe93d39600adfaaa8149fe5d025f33e47
Minju Kim <mindori1019@gmail.com>
2024-08-18 03:12:05 +0900

    feat/feed: schema 변경

--- src/main/kotlin/com/jordyma/blink/feed/dto/FeedIsMarkedResponseDto.kt
+++ src/main/kotlin/com/jordyma/blink/feed/dto/FeedIsMarkedResponseDto.kt
@@ -1,7 +1,15 @@
 package com.jordyma.blink.feed.dto
 
+import io.swagger.v3.oas.annotations.media.Schema
+
+@Schema(description = "피드 북마킹 성공 여부 응답 DTO")
 data class FeedIsMarkedResponseDto(
+    @Schema(description = "피드 ID")
     val id: Long,
+
+    @Schema(description = "북마킹 여부")
     val isMarked: Boolean,
+
+    @Schema(description = "수정일 타임스탬프")
     val modifiedDate: String
 )
