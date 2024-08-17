b0fdef2c1a42e090a6b6c323e97a24d9885598f4
Minju Kim <mindori1019@gmail.com>
2024-08-18 03:18:30 +0900

    feat/feed: 로그 추가

--- src/main/kotlin/com/jordyma/blink/feed/controller/FeedController.kt
+++ src/main/kotlin/com/jordyma/blink/feed/controller/FeedController.kt
@@ -2,21 +2,19 @@ package com.jordyma.blink.feed.controller
 
 import com.jordyma.blink.auth.jwt.user_account.UserAccount
 import com.jordyma.blink.feed.dto.*
-import com.jordyma.blink.global.resolver.RequestUserId
 import com.jordyma.blink.feed.service.FeedService
 import com.jordyma.blink.user.dto.UserInfoDto
 import com.jordyma.blink.user.service.UserService
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.Parameter
+import io.swagger.v3.oas.annotations.media.Schema
+import lombok.extern.java.Log
+import org.slf4j.LoggerFactory
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.security.core.annotation.AuthenticationPrincipal
-import org.springframework.web.bind.annotation.DeleteMapping
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PatchMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
+
 
 @RestController
 @RequestMapping("/api/feeds")
@@ -24,17 +22,26 @@ class FeedController(
     private val feedService: FeedService,
     private val userService: UserService
 ) {
-    @Operation(summary = "캘린더 피드 검색 api", description = "년도와 월(yyyy-MM)을 param으로 넣어주면, 해당 월의 피드들을 반환해줍니다.")
-    @GetMapping("by-date")
+    val log: org.slf4j.Logger = LoggerFactory.getLogger(Log::class.java)
+
+    @Operation(summary = "캘린더 피드 검색 api", description = "년도와 월(yyyy-MM)을 param으로 넣어주면, 해당 월의 피드들을 날짜를 Key로 반환해줍니다.")
+    @GetMapping("/by-date")
     fun getFeedsByDate(
+        @Schema(description = "년도와 월(yyyy-MM)", example = "2024-08")
         @RequestParam("yearMonth") yearMonth: String,
         @AuthenticationPrincipal userAccount: UserAccount
     ): ResponseEntity<FeedCalendarResponseDto> {
+        log.info("getFeedsByDate called : yearMonth = $yearMonth")
         val userDto: UserInfoDto = userService.find(userAccount.userId)
-        val response = feedService.getFeedsByMonth(user = userDto, yrMonth = yearMonth)
-        return ResponseEntity.ok(response)
-    }
+        try {
+            val response = feedService.getFeedsByMonth(user = userDto, yrMonth = yearMonth)
+            return ResponseEntity.ok(response)
+        } catch (e: Exception){
+            log.error("Error in getFeedsByMonth: ", e)
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null)
+        }
 
+    }
 
     @Operation(summary = "피드 상세 조회 api", description = "피드 아이디를 pathVariable로 넣어주면, 해당 피드id의 상세 정보를 반환해줍니다.")
     @GetMapping("/{feedId}")
@@ -42,6 +49,8 @@ class FeedController(
         @PathVariable("feedId") @Parameter(description = "피드 아이디", required = true) feedId: Long,
         @AuthenticationPrincipal userAccount: UserAccount
     ): ResponseEntity<FeedDetailDto> {
+        log.info("getFeedDetail called : feedId = $feedId")
+
         val userDto: UserInfoDto = userService.find(userAccount.userId)
         val feedDetailDto = feedService.getFeedDetail(user = userDto, feedId = feedId)
         return ResponseEntity.ok(feedDetailDto)
@@ -73,7 +82,7 @@ class FeedController(
     }
 
     @Operation(summary = "중요/미분류 피드 리스트 조회 api", description = "type은 BOOKMARKED / UNCLASSIFIED (String)으로 구분됩니다.")
-    @GetMapping("by-type")
+    @GetMapping("/by-type")
     fun getFeedsByType(
         @RequestParam("type") type: String,
         @RequestParam("page") page: Int,
@@ -87,15 +96,17 @@ class FeedController(
     }
 
     @Operation(summary = "피드 일반, 키워드 검색 api", description = "검색어를 param으로 넣어주면, 해당 검색어를 포함하는 피드 리스트를 반환해줍니다.")
-    @GetMapping("search")
+    @GetMapping("/search")
     fun searchFeeds(
         @RequestParam("query") query: String,
         @RequestParam("page") page: Int,
         @RequestParam("size") size: Int,
         @AuthenticationPrincipal userAccount: UserAccount
-    ): ResponseEntity<List<FeedResultDto>> {
+    ): ResponseEntity<FeedSearchResponseDto> {
+        log.info("searchFeeds called : query = $query")
         val userDto: UserInfoDto = userService.find(userAccount.userId)
         val responseDto = feedService.searchFeeds(user = userDto, query = query, page = page, size = size)
-        return ResponseEntity.ok(responseDto)
+        return ResponseEntity.ok(FeedSearchResponseDto(query = query, result = responseDto))
     }
+
 }
