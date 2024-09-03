0313ed011a0cc859783a4af8bc66e8b55a80d947
Minju Kim <mindori1019@gmail.com>
2024-09-03 23:08:41 +0900

    648116c  : resolve conflict

--- src/main/kotlin/com/jordyma/blink/global/exception/ErrorCode.kt
+++ src/main/kotlin/com/jordyma/blink/global/exception/ErrorCode.kt
@@ -31,4 +31,7 @@ enum class ErrorCode(val errorCode: String, val statusCode: HttpStatus) {
 
     // Feed
     FEED_NOT_FOUND("FD000", HttpStatus.NOT_FOUND),
+
+    // Token
+    TOKEN_EXCHANGE_FAILED("T000", HttpStatus.INTERNAL_SERVER_ERROR),
 }
