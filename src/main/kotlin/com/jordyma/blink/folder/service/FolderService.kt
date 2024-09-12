cc06ac6422447f31445f8c7da24c5c9a1dc6b561
daeunkwak <77181984+daeunkwak@users.noreply.github.com>
2024-09-13 00:48:58 +0900

    fix: RecommendRepository @Query 오류 수정

--- src/main/kotlin/com/jordyma/blink/folder/service/FolderService.kt
+++ src/main/kotlin/com/jordyma/blink/folder/service/FolderService.kt
@@ -17,7 +17,7 @@ import com.jordyma.blink.user.repository.UserRepository
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
-@Transactional(readOnly = true)
+@Transactional
 @Service
 class FolderService(
     private val folderRepository: FolderRepository,
