3cae9056200fc5d7f3c8f4f462c12d1683d7a5ad
Minju Kim <mindori1019@gmail.com>
2024-09-15 02:30:26 +0900

    feat/feed: FeedRepositoryCustom으로 함수 이동 (주석 내용 아예 지움)

--- src/main/kotlin/com/jordyma/blink/feed/repository/FeedRepository.kt
+++ src/main/kotlin/com/jordyma/blink/feed/repository/FeedRepository.kt
@@ -1,32 +1,9 @@
 package com.jordyma.blink.feed.repository
 
 import com.jordyma.blink.feed.entity.Feed
-import org.springframework.data.domain.Page
-import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.data.jpa.repository.Query
-import org.springframework.data.repository.query.Param
 
 interface FeedRepository : JpaRepository<Feed, Long>, FeedRepositoryCustom {
-
-//    @Query(
-//        "SELECT f FROM Feed f " +
-//          "JOIN f.keywords k " +
-//          "JOIN f.folder fo " +
-//         "WHERE fo.user.id = :userId " +  // userId로 필터링
-//           "AND f.folder.id = fo.id " +  // feed와 folder 연결
-//           "AND k.feed.id = f.id " +  // keyword와 feed 연결
-//           "AND (LOWER(f.title) LIKE LOWER(CONCAT('%', :query, '%')) " +  // title에 LIKE 검색
-//            "OR LOWER(f.summary) LIKE LOWER(CONCAT('%', :query, '%')) " +  // summary에 LIKE 검색
-//            "OR LOWER(f.memo) LIKE LOWER(CONCAT('%', :query, '%')) " +  // memo에 LIKE 검색
-//            "OR LOWER(k.content) LIKE LOWER(CONCAT('%', :query, '%')))" +// keyword에 LIKE 검색
-//           "AND f.deletedAt IS NULL"
-//    )
-//    fun findFeedByQuery(
-//        @Param("userId") userId: Long,
-//        @Param("query") query: String,
-//        pageable: Pageable
-//    ): Page<Feed>
 
 }
 
