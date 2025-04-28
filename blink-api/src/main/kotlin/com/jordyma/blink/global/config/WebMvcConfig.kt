dbf8ae50f3a242f0b423050010439c89eef55528
dawn <misodaeun3@naver.com>
2025-04-28 15:44:00 +0900

    feat: add UserActivityInterceptor

---
+++ blink-api/src/main/kotlin/com/jordyma/blink/global/config/WebMvcConfig.kt
@@ -0,0 +1,19 @@
+package com.jordyma.blink.global.config
+
+import com.jordyma.blink.infra.UserActivityInterceptor
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class WebMvcConfig : WebMvcConfigurer {
+
+    @Autowired
+    private lateinit var userActivityInterceptor: UserActivityInterceptor
+
+    override fun addInterceptors(registry: InterceptorRegistry) {
+        registry.addInterceptor(userActivityInterceptor)
+            .addPathPatterns("/api/**", "/user/**", "/auth/**")
+    }
+}
---
+++ blink-api/src/main/kotlin/com/jordyma/blink/infra/UserActivityInterceptor.kt
@@ -0,0 +1,50 @@
+package com.jordyma.blink.infra
+
+import com.jordyma.blink.auth.jwt.user_account.UserAccount
+import com.jordyma.blink.logger
+import com.jordyma.blink.stats.service.LinkStatisticsService
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
+import org.springframework.messaging.handler.HandlerMethod
+import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.stereotype.Component
+import org.springframework.web.servlet.HandlerInterceptor
+
+@Component
+class UserActivityInterceptor (
+    private val statisticsService: LinkStatisticsService,
+) : HandlerInterceptor {
+    override fun preHandle(request: HttpServletRequest, response: HttpServletResponse, handler: Any): Boolean {
+        val userId = getUserIdFromRequest()
+
+        // 활성 사용자수 ++
+        if (userId != null) {
+            statisticsService.recordUserActivity(userId)
+            logger().info("user record increment for user: {}", userId)
+        }
+
+        if (handler is HandlerMethod) {
+            val method = handler.method
+
+            // 피드 조회 api인 경우 ++
+            val requestURI = request.requestURI
+            if (requestURI.contains("/api/feeds/detail/") && request.method == "GET") {
+                logger().info("Feed view increment for URI: {}", requestURI)
+                statisticsService.incrementLinkView()
+            }
+        }
+
+        return true
+    }
+
+    private fun getUserIdFromRequest(): Long? {
+        val authentication = SecurityContextHolder.getContext().authentication
+
+        // Filter에서 주입했던 UserAccount 객체 활용
+        if (authentication != null && authentication.isAuthenticated && authentication.principal is UserAccount) {
+            val userAccount = authentication.principal as UserAccount
+            return userAccount.userId
+        }
+        return null
+    }
+}
